b66afca129db2861770a85853404bcd63a4d41f3
Bảo bùi <bao.btg@open-dev.io>
2024-06-06 18:34:50 +0700

    update main

--- app/(main)/home/page.tsx
+++ app/(main)/home/page.tsx
@@ -37,75 +37,7 @@ function HomePage() {
   //     true
   //   );
   // }, []);
-  return (
-    <>
-      {/* Banner section */}
-      <div className="relative h-170 w-full">
-        <Image
-          src={CommonConstants.images.homeBanner}
-          alt="Home banner"
-          fill
-          style={{
-            maxWidth: "100%",
-            objectFit: "cover",
-          }}
-        />
-
-        <Box
-          className="absolute inset-0 bg-black/50 flex flex-col justify-around"
-          px={CommonConstants.spacing.horizontalPadding}
-        >
-          <div className="flex flex-col text-white gap-5">
-            <Typography variant="h4" className="uppercase">
-              Welcome to
-            </Typography>
-            <Typography variant="h2" className="uppercase" fontWeight="bold">
-              SING STAY & CARE
-            </Typography>
-            <Typography variant="body1" fontWeight="bold">
-              Book your stay and enjoy amentities redefined at the most
-              affordable rates.
-            </Typography>
-          </div>
-
-          <div className="flex justify-center">
-            <Button
-              component={Link}
-              href="/contact-us"
-              variant="contained"
-              startIcon={<Phone />}
-              sx={{
-                px: 4,
-                py: 2,
-              }}
-            >
-              Contact us
-            </Button>
-          </div>
-        </Box>
-      </div>
-
-      <Box
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        px={CommonConstants.spacing.horizontalPadding}
-        py={CommonConstants.spacing.verticalPadding}
-      >
-        <div className={ANIMATION_REVEAL_ELEMENT.BOTTOM_TO_TOP.default}>
-          <AboutUs />
-        </div>
-        <Divider sx={{ height: "1px", width: "100%", mt: "60px" }} />
-        <div className={ANIMATION_REVEAL_ELEMENT.LEFT_TO_RIGHT.default}>
-          <RoomTypeList />
-        </div>
-        <Divider sx={{ height: "1px", width: "100%", mb: "60px" }} />
-        <Blog />
-        <Divider sx={{ height: "1px", width: "100%", mb: "60px" }} />
-        <TestimonialSection />
-      </Box>
-    </>
-  );
+  return <>{/* Banner section */}</>;
 }
 
 export default HomePage;
