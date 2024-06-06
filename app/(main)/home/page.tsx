073e16ddab3e9bc6410c5b3be2e4ae7bc1b4ca44
Bảo bùi <bao.btg@open-dev.io>
2024-06-06 18:31:32 +0700

    1111

--- app/(main)/home/page.tsx
+++ app/(main)/home/page.tsx
@@ -1,9 +1,9 @@
-"use client";
+// "use client";
 import AboutUs from "@/modules/home/sections/AboutUs";
 import Blog from "@/modules/home/sections/Blog";
 import RoomTypeList from "@/modules/home/sections/RoomTypeList";
 import TestimonialSection from "@/modules/home/sections/Testimonial";
-import { ScrollRevealElement } from "@/utils/animation";
+// import { ScrollRevealElement } from "@/utils/animation";
 import { ANIMATION_REVEAL_ELEMENT } from "@/utils/common";
 import { CommonConstants } from "@/utils/constants";
 import { Phone } from "@mui/icons-material";
@@ -13,30 +13,30 @@ import Link from "next/link";
 import { useEffect } from "react";
 
 function HomePage() {
-  useEffect(() => {
-    ScrollRevealElement(
-      ANIMATION_REVEAL_ELEMENT.BOTTOM_TO_TOP.default,
-      ANIMATION_REVEAL_ELEMENT.BOTTOM_TO_TOP.active
-    );
-    ScrollRevealElement(
-      ANIMATION_REVEAL_ELEMENT.LEFT_TO_RIGHT.default,
-      ANIMATION_REVEAL_ELEMENT.LEFT_TO_RIGHT.active
-    );
-    ScrollRevealElement(
-      ANIMATION_REVEAL_ELEMENT.RIGHT_TO_LEFT.default,
-      ANIMATION_REVEAL_ELEMENT.RIGHT_TO_LEFT.active
-    );
-    ScrollRevealElement(
-      ANIMATION_REVEAL_ELEMENT.LEFT_TO_RIGHT.default,
-      ANIMATION_REVEAL_ELEMENT.LEFT_TO_RIGHT.active,
-      true
-    );
-    ScrollRevealElement(
-      ANIMATION_REVEAL_ELEMENT.RIGHT_TO_LEFT.default,
-      ANIMATION_REVEAL_ELEMENT.RIGHT_TO_LEFT.active,
-      true
-    );
-  }, []);
+  // useEffect(() => {
+  //   ScrollRevealElement(
+  //     ANIMATION_REVEAL_ELEMENT.BOTTOM_TO_TOP.default,
+  //     ANIMATION_REVEAL_ELEMENT.BOTTOM_TO_TOP.active
+  //   );
+  //   ScrollRevealElement(
+  //     ANIMATION_REVEAL_ELEMENT.LEFT_TO_RIGHT.default,
+  //     ANIMATION_REVEAL_ELEMENT.LEFT_TO_RIGHT.active
+  //   );
+  //   ScrollRevealElement(
+  //     ANIMATION_REVEAL_ELEMENT.RIGHT_TO_LEFT.default,
+  //     ANIMATION_REVEAL_ELEMENT.RIGHT_TO_LEFT.active
+  //   );
+  //   ScrollRevealElement(
+  //     ANIMATION_REVEAL_ELEMENT.LEFT_TO_RIGHT.default,
+  //     ANIMATION_REVEAL_ELEMENT.LEFT_TO_RIGHT.active,
+  //     true
+  //   );
+  //   ScrollRevealElement(
+  //     ANIMATION_REVEAL_ELEMENT.RIGHT_TO_LEFT.default,
+  //     ANIMATION_REVEAL_ELEMENT.RIGHT_TO_LEFT.active,
+  //     true
+  //   );
+  // }, []);
   return (
     <>
       {/* Banner section */}
